4c74adabc286396e520e87d4383705750c3994c7
Owen Bowden (the404) <47672957+the404devs@users.noreply.github.com>
2025-06-13 10:59:54 -0400

    magic trick

--- js/app.js
+++ js/app.js
@@ -592,7 +592,8 @@ async function joinWorkspace(editorId, owner = user.uid) {
                     // continue; // Use continue to skip this iteration
 
                     // Attempt at purging old queue entries.
-                    docRef.update({`queue.${key}`: firebase.firestore.FieldValue.delete()});
+                    
+                    docRef.update({[`queue.${key}`]: firebase.firestore.FieldValue.delete()});
                 }
 
                 // Ignore changes made by this client
